81d5f0c24a61b584d9cd9bb19aad88c1d2ff4a66
João Ferraz (@p1padev) <jvkferraz@gmail.com>
2024-07-03 10:36:25 -0300

    Basic scoreboard logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import './App.css';
+import GamePanel from './components/GamePanel';
+import ScoreboardBar from './components/ScoreboardBar';
 
 function App() {
+  const [scoreboard, setScoreboard] = useState({
+    currentScore: 0,
+    bestScore: 0,
+  });
+
+  const increaseScore = () => {
+    const current = scoreboard.currentScore;
+    const best = scoreboard.bestScore;
+    setScoreboard({
+      currentScore: current + 1,
+      bestScore: current > best ? current : best,
+    });
+  };
+
+  const resetScore = () => {
+    setScoreboard({
+      ...scoreboard,
+      currentScore: 0,
+    });
+  };
+
   return (
     <>
-      <h1>Initial commit</h1>
+      <ScoreboardBar scoreboard={scoreboard}></ScoreboardBar>
+      <main className="game-container">
+        <GamePanel
+          increaseScore={increaseScore}
+          resetScore={resetScore}
+        ></GamePanel>
+      </main>
     </>
   );
 }
---
+++ src/components/ScoreboardBar.jsx
@@ -0,0 +1,3 @@
+export default function ScoreboardBar({ scoreboard }) {
+  return <header></header>;
+}
